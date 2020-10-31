c71037ccade96290625e44b877b0601ea01e2156
AlexShcherbak <alexshcherbak.dev@gmail.com>
2020-11-01 01:15:00 +0200

    1-10 tasks

---
+++ js/homework/js-1/script.js
@@ -0,0 +1,227 @@
+//---------1---------
+//Присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+
+
+// let hello = 'hello';
+// let owu = 'owu';
+// let com = 'com';
+// let ua = 'ua';
+// let one = 1;
+// let ten = 10;
+// let num = -999;
+// let number = 123;
+// let pi = 3.14;
+// let two = 2.7;
+// let sixt = 16;
+// let is = true;
+// let has = false;
+//
+// console.log(hello);
+// console.log(one);
+// console.log(pi);
+// console.log(is);
+// console.log(has);
+// // etc.
+
+//
+// alert(hello);
+// alert(one);
+// alert(pi);
+// alert(is);
+// alert(has);
+// etc.
+//
+// document.write(hello);
+// document.write(one);
+// document.write(pi);
+// document.write(is);
+// document.write(has);
+// // etc.
+//
+//
+// //---------2---------
+// Переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+//
+//
+// hello = 'hey';
+// one = 11;
+// pi = 3.1415;
+// is = false;
+// has = true;
+//
+// console.log(hello);
+// console.log(one);
+// console.log(pi);
+// console.log(is);
+// console.log(has);
+//
+// alert(hello);
+// alert(one);
+// alert(pi);
+// alert(is);
+// alert(has);
+//
+// document.write(hello, '</br>');
+// document.write(one, '</br>');
+// document.write(pi, '</br>');
+// document.write(is, '</br>');
+// document.write(has, '</br>');
+//
+//
+// //---------3---------
+// Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+//
+// const six = 6;
+// const three = 3;
+// const twenty = 20;
+//
+// const day = 'Monday';
+// const month = 'October';
+// const season = 'Autumn';
+//
+// console.log(six);
+// console.log(three);
+// console.log(twenty);
+//
+// console.log(day);
+// console.log(month);
+// console.log(season);
+//
+// alert(six);
+// alert(three);
+// alert(twenty);
+//
+// alert(day);
+// alert(month);
+// alert(season);
+//
+// document.write(six, '<br>');
+// document.write(three, '<br>' );
+// document.write(twenty, '<br>');
+//
+// document.write(day, '<br>');
+// document.write(month, '<br>');
+// document.write(season, '<br>');
+//
+//
+// //---------4---------
+// При помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные. Вывести каждую при помощи console.log , alert, document.write
+//
+//
+// let surname = prompt('Прізвище');
+// let name = prompt('Імя');
+// let patronymic = prompt('Імя по батькові');
+// //
+// console.log(surname + " " + name + " " + patronymic);
+// console.log(`${surname} ${name} ${patronymic}`);
+// alert(surname + " " + name + " " + patronymic);
+// alert(`${surname} ${name} ${patronymic}`);
+// document.write(surname + " " + name + " " + patronymic);
+// document.write(`${surname} ${name} ${patronymic}`);
+//
+//
+//---------5---------
+//Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+//
+//
+// let person = `${surname} ${name} ${patronymic}`;
+// console.log(person);
+//
+//
+//---------6---------
+// Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+//
+//
+// let family = {
+//     father: {
+//         name: prompt('Імя'),
+//         surname: prompt('Прізвище'),
+//         patronymic: prompt('Імя по батькові'),
+//     },
+//     mother: {
+//         name: prompt('Імя'),
+//         surname: prompt('Прізвище'),
+//         patronymic: prompt('Імя по батькові'),
+//     },
+//     son: {
+//         name: prompt('Імя'),
+//         surname: prompt('Прізвище'),
+//         patronymic: prompt('Імя по батькові'),
+//     }
+// };
+//
+// console.log(family);
+//
+//
+//---------7---------
+//При помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+//
+//
+// let firstNumber = +prompt();
+// let secondNumber = +prompt();
+// let thirdNumber = +prompt();
+//
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(thirdNumber);
+//
+//
+//---------8---------
+//При помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+//
+//
+// let firstNumber = parseInt(prompt());
+// let secondNumber = parseInt(prompt());
+// let thirdNumber = parseInt(prompt());
+// let fourthNumber = parseInt(prompt());
+//
+// console.log(firstNumber);
+// console.log(secondNumber);
+// console.log(thirdNumber);
+// console.log(fourthNumber);
+//
+// let result = `${firstNumber} ${secondNumber} ${thirdNumber} ${fourthNumber}`;
+// let result2 = firstNumber + secondNumber + thirdNumber + fourthNumber;
+// console.log(result);
+// console.log(result2);
+//
+//
+//---------9---------
+// При помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+//
+//
+// let firstNumber = parseFloat(prompt());
+// let secondNumber = parseFloat(prompt());
+// let thirdNumber = parseFloat(prompt());
+// let fourthNumber = parseFloat(prompt());
+//
+// let result = `${firstNumber} ${secondNumber} ${thirdNumber} ${fourthNumber}`;
+// let result2 = firstNumber + secondNumber + thirdNumber + fourthNumber;
+//
+// console.log(result);
+// console.log(result2);
+//
+//
+//---------10---------
+//При помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+//
+//
+// let firstNumber = parseFloat(prompt());
+// let secondNumber = parseFloat(prompt());
+// let thirdNumber = parseFloat(prompt());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
