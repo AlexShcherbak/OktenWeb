bbfc6466f84160e35009c36c015ff21d0097d780
AlexShcherbak <alexshcherbak.dev@gmail.com>
2020-11-14 02:40:39 +0200

    js6

---
+++ js/homework/js-6/main.js
@@ -0,0 +1,40 @@
+// - создать массив с 20 числами.
+let numbers = [1, 2, 43, 54, 12, 33, 55, 122, 42, 554, 123, 55, 95, 70, 833, 633, 6321, 252, 16, 20];
+
+// -- при помощи метода sort и колбека  отсортировать массив.
+// let sortNumber = numbers.sort();
+// console.log(sortNumber);
+
+// -- при помощи метода sort и колбека отсортировать массив в ниспадающем напралении.
+// numbers.sort(function (a, b) {
+//     return a-b; // return b-a
+// })
+// console.log(numbers);
+
+// -- при помощи filter получить числа кратные 3
+// let filter = numbers.filter(function (number) {
+//     return number % 3 === 0;
+// })
+// console.log(filter);
+
+// -- при помощи filter получить числа кратные 10
+// let filter = numbers.filter(function (number) {
+//     return number % 10 === 0;
+// })
+//
+// console.log(filter);
+
+// -- перебрать (проитерировать) массив при помощи foreach()
+// numbers.forEach(number => console.log(number));
+
+// -- перебрать массив при помощи map() и получить новый массив в котором все значения будут в 3 раза больше
+// let map = numbers.map()
+
+
+
+
+
+
+
+
+
