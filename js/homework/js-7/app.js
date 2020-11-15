57451d9cf23dcd10d9bf17d9b19b928ab95398eb
AlexShcherbak <alexshcherbak.dev@gmail.com>
2020-11-16 01:41:47 +0200

    js7

---
+++ js/homework/js-7/app.js
@@ -0,0 +1,61 @@
+// - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так,
+// чтобы при клике на кнопку исчезал элемент с id="text".
+
+// let createH2 = document.createElement('h1');
+// createH2.innerText = 'Hello World';
+// document.body.appendChild(createH2);
+// createH2.id = 'text';
+//
+// createH2.onclick = () => {
+//     createH2.style.display = 'none';
+// }
+
+
+// Создайте кнопку, при клике на которую, она будет скрывать сама себя.?
+
+// let button = document.createElement('button');
+// document.body.appendChild(button);
+// button.innerText = 'Оно тебе сожрет))0)0)0)'
+// button.onclick = () => button.style.display = 'none';
+// button.style.backgroundColor = 'red';
+// button.style.padding = '10px';
+
+
+
+
+// створити інпут який приймає вік людини та кнопку яка підтверджує дію.
+// При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18,
+// та повідомити про це користувача
+
+// let createForm = document.createElement('form')
+//
+// let createInput = document.createElement('input')
+// createInput.setAttribute('type', 'value')
+//
+// let createSubmitInput = document.createElement('input')
+// let submitInput = createSubmitInput.setAttribute('type', 'submit')
+// createSubmitInput.value = 'Підтвердити'
+//
+// document.body.appendChild(createForm)
+//
+// createForm.appendChild(createInput)
+// createForm.appendChild(createSubmitInput)
+//
+// createSubmitInput.onclick = () => {
+//     if(createInput.value < 18) {
+//         alert('Loser')
+//     } else {
+//         alert('Winner')
+//     }
+// }
+// // ----------------------------------------------------------------------- автоматизувати--------------------
+
+
+// Создайте меню, которое раскрывается/сворачивается при клике
+
+
+
+
+
+
+
